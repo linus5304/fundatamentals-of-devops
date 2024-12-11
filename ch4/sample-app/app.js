e2bf0deec31af6b6bd91a3f4fdb317bb5c57b035
linusbayere <linusbayere@gmail.com>
2024-12-11 00:16:05 -0500

    Add ch4 sample code

--- ch4/sample-app/app.js
+++ ch4/sample-app/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.get("/name/:name", (req, res) => {
+  res.render("hello", { name: req.params.name });
+});
+
 app.get("/health", (req, res) => {
   res.send("Healthy");
 });
